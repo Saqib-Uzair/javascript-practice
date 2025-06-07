b65f0692ddb725113d39984de0c00d0b28f1c316
Saqib Uzair <89041654+Saqib-Uzair@users.noreply.github.com>
2025-06-07 11:43:27 +0500

    Add files via upload

---
+++ JS/numbers/demo.js
@@ -0,0 +1,22 @@
+// let x = 100/"apple";
+// let y = 20;
+// let z = 30;
+
+// let result = x + y + z;
+
+// console.log(isNaN(x));
+
+
+
+// let x = 9.656;
+let y = 123;
+// console.log(x.toExponential(2));
+// console.log(x.toExponential(4));
+
+// console.log(x.toFixed(0))
+// console.log(x.toFixed(2))
+// console.log(x.toFixed(4))
+
+// console.log(y.valueOf());
+let x = Number.MIN_VALUE;
+console.log(x);
